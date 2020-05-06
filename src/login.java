1af5cd277dc1f341898d5ca42d20e51278149be4
lts931221 <w609902376@qq.com>
2020-05-06 15:07:56 +0800

    Initial commit

---
+++ src/login.java
@@ -0,0 +1,35 @@
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class login {
+    static WebDriver Driver;
+
+    @BeforeClass
+    public static void beforeClass() {
+        //设置驱动所在位置
+        System.setProperty("webdriver.firefox.driver", "C:\\Program Files\\Mozilla Firefox\\Firefox.exe");
+        System.setProperty("webdriver.gecko.driver", "C:\\Program Files\\Mozilla Firefox\\geckodriver.exe");
+        //引用火狐浏览器驱动
+
+        Driver = new FirefoxDriver();
+        Driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        System.out.println("用例开始执行…………\n");
+        Driver.manage().window().maximize();
+    }
+
+    @Test//后台登录
+    public  void admin_login() {
+        Driver.get("http://admin.xinanyuncai.com/login");
+        try {
+            Thread.sleep(2000);
+            System.out.println("测试成功");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
